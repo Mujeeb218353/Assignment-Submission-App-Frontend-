22e6179435af11d0d47f5fe077059be816e940a4
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-09-13 01:51:51 +0500

    Solved Authentication Bug

--- src/hooks/useAuthRedirect.js
+++ src/hooks/useAuthRedirect.js
@@ -2,17 +2,16 @@ import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const useAuthRedirect = () => {
-
+  const isAuthenticated = false;
   const navigate = useNavigate();
   const location = useLocation();
   useEffect(() => {
-    if (location.pathname === '/') {
+    if (location.pathname !== '/login' || location.pathname !== '/signup') {
       if (!(localStorage.getItem('my-accessToken'))) {
         navigate('/login');
       }
     }
   }, [location, navigate]);
-  
 };
 
 export default useAuthRedirect;
